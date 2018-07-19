1f1075aca8af8b4aa36e2fea98675662605bef03
Andrea Maria Piana <andrea.maria.piana@gmail.com>
2018-07-19 10:33:43 +0200

    linting

--- x3dh/x3dh.go
+++ x3dh/x3dh.go
@@ -1,139 +1,139 @@
 package x3dh
 
 import (
-  "fmt"
-  "errors"
-  "crypto/ecdsa"
+	"crypto/ecdsa"
+	"errors"
 
-  "github.com/golang/protobuf/jsonpb"
-  "github.com/ethereum/go-ethereum/crypto"
-  "github.com/ethereum/go-ethereum/crypto/ecies"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/ecies"
+	"github.com/golang/protobuf/jsonpb"
 )
 
 const (
-  sskLen = 16
+	sskLen = 16
 )
 
 func (bundle Bundle) ToJSON() (string, error) {
-  ma := jsonpb.Marshaler{};
-  return ma.MarshalToString(&bundle);
+	ma := jsonpb.Marshaler{}
+	return ma.MarshalToString(&bundle)
 }
 
 func FromJSON(str string) (*Bundle, error) {
-  var bundle Bundle;
-  err := jsonpb.UnmarshalString(str, &bundle);
-  return &bundle, err
+	var bundle Bundle
+	err := jsonpb.UnmarshalString(str, &bundle)
+	return &bundle, err
 }
 
-func NewBundle(identity *ecdsa.PrivateKey) (* Bundle, * ecdsa.PrivateKey, error) {
-  preKey, err := crypto.GenerateKey()
+func NewBundle(identity *ecdsa.PrivateKey) (*Bundle, *ecdsa.PrivateKey, error) {
+	preKey, err := crypto.GenerateKey()
 
-  if err != nil {
-    return nil, nil, err
-  }
+	if err != nil {
+		return nil, nil, err
+	}
 
-  compressedPreKey  := crypto.CompressPubkey(&preKey.PublicKey)
-  compressedIdentityKey  := crypto.CompressPubkey(&identity.PublicKey)
+	compressedPreKey := crypto.CompressPubkey(&preKey.PublicKey)
+	compressedIdentityKey := crypto.CompressPubkey(&identity.PublicKey)
 
-  signature, err := crypto.Sign(crypto.Keccak256(compressedPreKey), identity)
+	signature, err := crypto.Sign(crypto.Keccak256(compressedPreKey), identity)
 
-  if err != nil {
-    return nil, nil, err
-  }
+	if err != nil {
+		return nil, nil, err
+	}
 
-  bundle := Bundle {
-    Identity: compressedIdentityKey,
-    SignedPreKey: compressedPreKey,
-    Signature: signature,
-  }
+	bundle := Bundle{
+		Identity:     compressedIdentityKey,
+		SignedPreKey: compressedPreKey,
+		Signature:    signature,
+	}
 
-  return &bundle, preKey, nil
+	return &bundle, preKey, nil
 }
 
 func verifyBundle(bundle *Bundle, bundleIdentityKey *ecdsa.PublicKey) error {
 
-  recoveredKey, err := crypto.SigToPub(
-    crypto.Keccak256(bundle.GetSignedPreKey()),
-    bundle.GetSignature(),
-  )
+	recoveredKey, err := crypto.SigToPub(
+		crypto.Keccak256(bundle.GetSignedPreKey()),
+		bundle.GetSignature(),
+	)
 
-  if err != nil {
-    return err
-  }
+	if err != nil {
+		return err
+	}
 
-  fmt.Printf("%x %x", recoveredKey.Y, bundleIdentityKey.Y);
+	if crypto.PubkeyToAddress(*recoveredKey) != crypto.PubkeyToAddress(*bundleIdentityKey) {
+		return errors.New("Identity key and signature mismatch")
+	}
 
-  if crypto.PubkeyToAddress(*recoveredKey) != crypto.PubkeyToAddress(*bundleIdentityKey) {
-    return errors.New("Identity key and signature mismatch")
-  }
-
-  return nil
+	return nil
 }
 
 func x3dh(
-  bundleIdentityKey *ecies.PublicKey,
-  bundleSignedPreKey *ecies.PublicKey,
-  myIdentityKey *ecies.PrivateKey,
-  myEphemeralKey *ecies.PrivateKey,
+	bundleIdentityKey *ecies.PublicKey,
+	bundleSignedPreKey *ecies.PublicKey,
+	myIdentityKey *ecies.PrivateKey,
+	myEphemeralKey *ecies.PrivateKey,
 ) ([]byte, error) {
-  dh1, err := myIdentityKey.GenerateShared(
-    bundleSignedPreKey,
-    sskLen,
-    sskLen,
-  )
-  if err != nil {
-    return nil, err
-  }
+	dh1, err := myIdentityKey.GenerateShared(
+		bundleSignedPreKey,
+		sskLen,
+		sskLen,
+	)
+	if err != nil {
+		return nil, err
+	}
 
-  dh2, err := myEphemeralKey.GenerateShared(
-    bundleIdentityKey,
-    sskLen,
-    sskLen,
-  )
-  if err != nil {
-    return nil, err
-  }
+	dh2, err := myEphemeralKey.GenerateShared(
+		bundleIdentityKey,
+		sskLen,
+		sskLen,
+	)
+	if err != nil {
+		return nil, err
+	}
 
-  dh3, err := myEphemeralKey.GenerateShared(
-    bundleSignedPreKey,
-    sskLen,
-    sskLen,
-  )
-  if err != nil {
-    return nil, err
-  }
+	dh3, err := myEphemeralKey.GenerateShared(
+		bundleSignedPreKey,
+		sskLen,
+		sskLen,
+	)
+	if err != nil {
+		return nil, err
+	}
 
-  secretInput := append(append(dh1, dh2...), dh3...)
+	secretInput := append(append(dh1, dh2...), dh3...)
 
-  sharedSecret := crypto.Keccak256(secretInput)
+	sharedSecret := crypto.Keccak256(secretInput)
 
-  return sharedSecret, nil
+	return sharedSecret, nil
 }
 
 func ProcessBundle(bundle *Bundle, prv *ecdsa.PrivateKey) ([]byte, error) {
 
-  bundleIdentityKey, err := crypto.DecompressPubkey(bundle.GetIdentity())
-  if err != nil {
-    return nil, err
-  }
+	bundleIdentityKey, err := crypto.DecompressPubkey(bundle.GetIdentity())
+	if err != nil {
+		return nil, err
+	}
 
-  bundleSignedPreKey, err := crypto.DecompressPubkey(bundle.GetIdentity())
-  if err != nil {
-    return nil, err
-  }
+	bundleSignedPreKey, err := crypto.DecompressPubkey(bundle.GetIdentity())
+	if err != nil {
+		return nil, err
+	}
 
-  err = verifyBundle(bundle, bundleIdentityKey);
+	err = verifyBundle(bundle, bundleIdentityKey)
 
-  if err != nil {
-    return nil, err
-  }
+	if err != nil {
+		return nil, err
+	}
 
-  ephemeralKey1, err := crypto.GenerateKey()
+	ephemeralKey1, err := crypto.GenerateKey()
+	if err != nil {
+		return nil, err
+	}
 
-  return x3dh(
-    ecies.ImportECDSAPublic(bundleIdentityKey),
-    ecies.ImportECDSAPublic(bundleSignedPreKey),
-    ecies.ImportECDSA(prv),
-    ecies.ImportECDSA(ephemeralKey1),
-  )
+	return x3dh(
+		ecies.ImportECDSAPublic(bundleIdentityKey),
+		ecies.ImportECDSAPublic(bundleSignedPreKey),
+		ecies.ImportECDSA(prv),
+		ecies.ImportECDSA(ephemeralKey1),
+	)
 }
--- x3dh/x3dh_test.go
+++ x3dh/x3dh_test.go
@@ -1,152 +1,155 @@
 package x3dh
 
 import (
-  "testing"
-  "strings"
+	"strings"
+	"testing"
 
-  "encoding/hex"
+	"encoding/hex"
 
-  "github.com/ethereum/go-ethereum/crypto/ecies"
-  "github.com/ethereum/go-ethereum/crypto"
-  "github.com/stretchr/testify/assert"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/crypto/ecies"
+	"github.com/stretchr/testify/assert"
 )
 
 const (
-  privateKey1 = "00000000000000000000000000000000"
-  privateKey2 = "11111111111111111111111111111111"
+	privateKey1 = "00000000000000000000000000000000"
+	privateKey2 = "11111111111111111111111111111111"
 
-  pubKey = "04760c4460e5336ac9bbd87952a3c7ec4363fc0a97bd31c86430806e287b437fd1b01abc6e1db640cf3106b520344af1d58b00b57823db3e1407cbc433e1b6d04d"
-  jsonBundle = "{\"identity\":\"Ai7VV/WtM2sxpJhX5OlmSVSsMzhaogqT4tZL/n8I9RJ3\",\"signedPreKey\":\"A3YMRGDlM2rJu9h5UqPH7ENj/AqXvTHIZDCAbih7Q3/R\",\"signature\":\"V/WbDiANpbmAV8aB3KUKDMBP/htduOdcpGbA9rcJljNTDATz15iUTaD80Lge653ER2JTihIy01DjAo7fhjk/WwE=\"}"
+	pubKey     = "04760c4460e5336ac9bbd87952a3c7ec4363fc0a97bd31c86430806e287b437fd1b01abc6e1db640cf3106b520344af1d58b00b57823db3e1407cbc433e1b6d04d"
+	jsonBundle = "{\"identity\":\"Ai7VV/WtM2sxpJhX5OlmSVSsMzhaogqT4tZL/n8I9RJ3\",\"signedPreKey\":\"A3YMRGDlM2rJu9h5UqPH7ENj/AqXvTHIZDCAbih7Q3/R\",\"signature\":\"V/WbDiANpbmAV8aB3KUKDMBP/htduOdcpGbA9rcJljNTDATz15iUTaD80Lge653ER2JTihIy01DjAo7fhjk/WwE=\"}"
 )
 
 var sharedKey = []byte{0x42, 0xe9, 0xa9, 0x42, 0xdb, 0xc9, 0x32, 0x9e, 0xaa, 0x3d, 0x3f, 0xa7, 0x75, 0x56, 0x7f, 0x4a, 0x3f, 0x8d, 0x2e, 0xb, 0xb0, 0x6b, 0x67, 0x3a, 0x97, 0x79, 0x76, 0xd6, 0xa5, 0x9b, 0x5f, 0xa9}
 
 func testBundle() (*Bundle, error) {
-  privateKey, err := crypto.ToECDSA([]byte(privateKey1))
-  if err != nil {
-    return nil, err
-  }
+	privateKey, err := crypto.ToECDSA([]byte(privateKey1))
+	if err != nil {
+		return nil, err
+	}
 
-  byteSignedPreKey, err := hex.DecodeString("04760c4460e5336ac9bbd87952a3c7ec4363fc0a97bd31c86430806e287b437fd1b01abc6e1db640cf3106b520344af1d58b00b57823db3e1407cbc433e1b6d04d")
-  if err != nil {
-    return nil, err
-  }
+	byteSignedPreKey, err := hex.DecodeString(pubKey)
+	if err != nil {
+		return nil, err
+	}
 
-  signedPreKey, err := crypto.UnmarshalPubkey([]byte(byteSignedPreKey))
-  if err != nil {
-    return nil, err
-  }
+	signedPreKey, err := crypto.UnmarshalPubkey(byteSignedPreKey)
+	if err != nil {
+		return nil, err
+	}
 
-  compressedPreKey := crypto.CompressPubkey(signedPreKey)
+	compressedPreKey := crypto.CompressPubkey(signedPreKey)
 
-  signature, err := crypto.Sign(crypto.Keccak256(compressedPreKey), privateKey)
+	signature, err := crypto.Sign(crypto.Keccak256(compressedPreKey), privateKey)
+	if err != nil {
+		return nil, err
+	}
 
-  bundle := Bundle {
-    Identity: crypto.CompressPubkey(&privateKey.PublicKey),
-    SignedPreKey: compressedPreKey,
-    Signature: signature,
-  }
+	bundle := Bundle{
+		Identity:     crypto.CompressPubkey(&privateKey.PublicKey),
+		SignedPreKey: compressedPreKey,
+		Signature:    signature,
+	}
 
-  return &bundle, nil;
+	return &bundle, nil
 }
 
-
 func TestNewBundle(t *testing.T) {
-  privateKey, err := crypto.ToECDSA([]byte(strings.Repeat("0", 32)))
+	privateKey, err := crypto.ToECDSA([]byte(strings.Repeat("0", 32)))
 
-  assert.Nil(t, err, "Private key should be generated without errors")
+	assert.Nil(t, err, "Private key should be generated without errors")
 
-  bundle, _, err := NewBundle(privateKey)
+	bundle, _, err := NewBundle(privateKey)
 
-  assert.Nil(t, err, "Bundle should be generated without errors")
+	assert.Nil(t, err, "Bundle should be generated without errors")
 
-  recoveredPublicKey, err := crypto.SigToPub(
-    crypto.Keccak256(bundle.GetSignedPreKey()),
-    bundle.Signature,
-  )
+	recoveredPublicKey, err := crypto.SigToPub(
+		crypto.Keccak256(bundle.GetSignedPreKey()),
+		bundle.Signature,
+	)
 
-  assert.Nil(t, err, "Public key should be recovered from the bundle successfully")
+	assert.Nil(t, err, "Public key should be recovered from the bundle successfully")
 
-  assert.Equalf(
-    t,
-    &privateKey.PublicKey,
-    recoveredPublicKey,
-    "The correct public key should be recovered",
-  )
+	assert.Equalf(
+		t,
+		&privateKey.PublicKey,
+		recoveredPublicKey,
+		"The correct public key should be recovered",
+	)
 }
 
 func TestToJSON(t *testing.T) {
 
-  bundle, err := testBundle()
+	bundle, err := testBundle()
 
-  assert.Nil(t, err, "Test bundle should be generated without errors")
+	assert.Nil(t, err, "Test bundle should be generated without errors")
 
-  actualJsonBundle, err := bundle.ToJSON()
+	actualJSONBundle, err := bundle.ToJSON()
 
-  assert.Nil(t, err, "no error should be reported")
+	assert.Nil(t, err, "no error should be reported")
 
-  assert.Equalf(
-    t,
-    jsonBundle,
-    actualJsonBundle,
-    "The correct bundle should be generated",
-  )
+	assert.Equalf(
+		t,
+		jsonBundle,
+		actualJSONBundle,
+		"The correct bundle should be generated",
+	)
 }
 
 func TestFromJSON(t *testing.T) {
 
-  expectedBundle, err := testBundle()
+	expectedBundle, err := testBundle()
+	assert.Nil(t, err, "Test bundle should be generated without errors")
 
-  assert.Nil(t, err, "Test bundle should be generated without errors")
+	actualBundle, err := FromJSON(jsonBundle)
 
-  actualBundle, err := FromJSON(jsonBundle)
+	assert.Nil(t, err, "Bundle should be unmarshaled without errors")
 
-  assert.Nil(t, err, "Bundle should be unmarshaled without errors")
-
-  assert.Equalf(
-    t,
-    expectedBundle,
-    actualBundle,
-    "The correct bundle should be generated",
-  )
+	assert.Equalf(
+		t,
+		expectedBundle,
+		actualBundle,
+		"The correct bundle should be generated",
+	)
 }
 
 func TestX3dh(t *testing.T) {
-  bundle, err := testBundle()
+	bundle, err := testBundle()
+	assert.Nil(t, err, "Test bundle should be generated without errors")
 
-  bundleIdentityKey, err := crypto.DecompressPubkey(bundle.GetIdentity())
-  assert.Nil(t, err, "Bundle identity key should be generated without errors")
+	bundleIdentityKey, err := crypto.DecompressPubkey(bundle.GetIdentity())
+	assert.Nil(t, err, "Bundle identity key should be generated without errors")
 
-  bundleSignedPreKey, err := crypto.DecompressPubkey(bundle.GetIdentity())
-  assert.Nil(t, err, "Bundle signed pre key should be generated without errors")
+	bundleSignedPreKey, err := crypto.DecompressPubkey(bundle.GetIdentity())
+	assert.Nil(t, err, "Bundle signed pre key should be generated without errors")
 
-  privateKey, err := crypto.ToECDSA([]byte(privateKey1))
-  assert.Nil(t, err, "private key should be generated without errors")
+	privateKey, err := crypto.ToECDSA([]byte(privateKey1))
+	assert.Nil(t, err, "private key should be generated without errors")
 
-  ephemeralKey1, err := crypto.ToECDSA([]byte(privateKey1))
-  assert.Nil(t, err, "ephemeral key should be generated without errors")
+	ephemeralKey1, err := crypto.ToECDSA([]byte(privateKey1))
+	assert.Nil(t, err, "ephemeral key should be generated without errors")
 
-  x3dh, err := x3dh(
-    ecies.ImportECDSAPublic(bundleIdentityKey),
-    ecies.ImportECDSAPublic(bundleSignedPreKey),
-    ecies.ImportECDSA(privateKey),
-    ecies.ImportECDSA(ephemeralKey1),
-  )
+	x3dh, err := x3dh(
+		ecies.ImportECDSAPublic(bundleIdentityKey),
+		ecies.ImportECDSAPublic(bundleSignedPreKey),
+		ecies.ImportECDSA(privateKey),
+		ecies.ImportECDSA(ephemeralKey1),
+	)
 
-  assert.Equalf(t, sharedKey, x3dh, "Should generate the correct key")
+	assert.Nil(t, err, "Shared key should be generated without errors")
+	assert.Equalf(t, sharedKey, x3dh, "Should generate the correct key")
 }
 
 func TestProcessBundle(t *testing.T) {
-  bundle, err := testBundle()
+	bundle, err := testBundle()
 
-  assert.Nil(t, err, "Test bundle should be generated without errors")
+	assert.Nil(t, err, "Test bundle should be generated without errors")
 
-  privateKey, err := crypto.ToECDSA([]byte(privateKey2))
+	privateKey, err := crypto.ToECDSA([]byte(privateKey2))
 
-  assert.Nil(t, err, "Private key should be imported without errors")
+	assert.Nil(t, err, "Private key should be imported without errors")
 
-  actual, err := ProcessBundle(bundle, privateKey)
+	actual, err := ProcessBundle(bundle, privateKey)
 
-  assert.Nil(t, err, "no error should be reported")
-  assert.NotNil(t, actual, "A key should be generated")
+	assert.Nil(t, err, "no error should be reported")
+	assert.NotNil(t, actual, "A key should be generated")
 }
